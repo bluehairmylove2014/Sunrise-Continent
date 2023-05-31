669219feb4b6229da203f08b9bee8669b64df6a6
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2023-05-31 22:01:35 +0700

    layout header

--- sunrise-continent/src/components/layouts/Header.js
+++ sunrise-continent/src/components/layouts/Header.js
@@ -10,16 +10,16 @@ const Header = () => {
     return (
         <header className='header'>
             <div className="header__logo-container">
-                <img 
+                <img
                     src={logo_img}
-                    alt="Sunrise Continent" 
-                    className="header__logo-img" 
+                    alt="Sunrise Continent"
+                    className="header__logo-img"
                 />
             </div>
             <div className="header__search">
                 <form>
                     <i className="fi fi-rr-search"></i>
-                    <input type="text" placeholder='In which location are you looking for a hotel room?'/>
+                    <input type="text" placeholder='In which location are you looking for a hotel room?' />
                 </form>
                 <nav className="header__main-nav">
                     <ul className='header-main-nav__infor'>
@@ -39,18 +39,48 @@ const Header = () => {
                         </li>
                     </ul>
                     <ul className='header-main-nav__social-link'>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>
+                            <a rel="noreferrer" target="_blank" href="https://www.facebook.com/MinMinPD2211/">
+                                <i class="fi fi-brands-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a rel="noreferrer" target="_blank" href="https://www.instagram.com/phucdat_dm/">
+                                <i class="fi fi-brands-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a rel="noreferrer" target="_blank" href="https://www.youtube.com/channel/UCD2T2jAlO282XPmDfftRpvw">
+                                <i class="fi fi-brands-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/phan-ph%C3%BAc-%C4%91%E1%BA%A1t-b88871243/">
+                                <i class="fi fi-brands-linkedin"></i>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
             <nav className="header__product-nav">
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>
+                        <button>
+                            Destination
+                            <i class="fi fi-rr-caret-down"></i>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            Trending
+                            <i class="fi fi-rr-caret-down"></i>
+                        </button>
+                    </li>
                 </ul>
+                <button className='product-nav__wist-list'>
+                    <i class="fi fi-rs-heart"></i>
+                    Wist List
+                </button>
             </nav>
         </header>
     );

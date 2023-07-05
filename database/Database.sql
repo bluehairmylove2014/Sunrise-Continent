645e406cee26968700fd7ad34c9e080c5ee14767
cuong457 <pnccuong20@clc.fitus.edu.vn>
2023-07-05 22:23:37 +0700

    Update: table columns name & create new procs

--- database/Database.sql
+++ database/Database.sql
@@ -1,129 +1,142 @@
 ﻿USE MASTER
 GO
 
-IF DB_ID('Sunrise_continent_db') IS NOT NULL
-	DROP DATABASE Sunrise_continent_db
+IF DB_ID('SunriseServer') IS NOT NULL
+	DROP DATABASE SunriseServer
 GO
 
-CREATE DATABASE Sunrise_continent_db
+CREATE DATABASE SunriseServer
 GO
 
-USE Sunrise_continent_db
+USE SunriseServer
 GO
 
-
---CREATE TABLE HOTEL_LOCATION
---(
---	country NVARCHAR(20),
---	province_city NVARCHAR(20),
-
---	PRIMARY KEY (country, province_city)
---)
 
 --!Hotel và Room
 CREATE TABLE HOTEL
 (
-	hotel_id INTEGER,
-	hotel_name NVARCHAR(100),
-	country NVARCHAR(20), -- not null
-	province_city NVARCHAR(20), -- not null
-	full_address NVARCHAR(100),
-	stars INT,
-	rating FLOAT,
-	hotel_description NVARCHAR(1000),
-	preview_img NVARCHAR(1000),
-	PRIMARY KEY (hotel_id)
+	Id INTEGER,
+	Name NVARCHAR(100),
+	Country NVARCHAR(20), -- not null
+	ProvinceCity NVARCHAR(20), -- not null
+	Address NVARCHAR(100),
+	Stars INT,
+	Rating FLOAT,
+	Description NVARCHAR(1000),
+	Image NVARCHAR(1000),
+
+	PRIMARY KEY (Id)
 )
 
 
 CREATE TABLE ROOM_TYPE
 (
-	hotel_id INTEGER,
-	room_type_id INTEGER,
-	room_type_name NVARCHAR(100),
-	vacancy INTEGER,
-	size FLOAT,
-	price FLOAT,
-	room_info NVARCHAR(1000),
-	room_view NVARCHAR(1000),
-	facilities NVARCHAR(1000),
+	HotelId INTEGER,
+	Id INTEGER,
+	Name NVARCHAR(100),
+	Vacancy INTEGER,
+	Size FLOAT,
+	Price FLOAT,
+	RoomInfo NVARCHAR(1000),
+	RoomView NVARCHAR(1000),
+	Facilites NVARCHAR(1000),
 	Smoking NVARCHAR(100),
-	PRIMARY KEY (hotel_id, room_type_id)
+
+	PRIMARY KEY (HotelId, Id)
 )
 
 
 CREATE TABLE ROOM_PICTURE
 (
-	hotel_id INTEGER,
-	room_type_id INTEGER,
-	picture_id INTEGER,
-	picture_link VARCHAR(1000),
+	HotelId INTEGER,
+	RoomTypeId INTEGER,
+	PictureId INTEGER,
+	PictureLink VARCHAR(1000),
 
-	PRIMARY KEY (hotel_id, room_type_id, picture_id)
+	PRIMARY KEY (HotelId, RoomTypeId, PictureId)
 )
 
 --! Acco và payment method
 CREATE TABLE ACCOUNT
 (
-	account_id INTEGER,
-	account_email VARCHAR(50),
-	account_password VARCHAR(50),
-	member_points INTEGER,
-	PRIMARY KEY (account_id)
+	Id INTEGER,
+    MemberPoint INTEGER,
+        
+    Username VARCHAR(50),
+    PasswordHash VARBINARY(100),
+    PasswordSalt VARBINARY(100),
+    RefreshToken VARCHAR(100),
+    TokenCreated DATETIME,
+    TokenExpires DATETIME,
+
+	PRIMARY KEY (Id)
 )
 
 CREATE TABLE PERSONAL_DETAILS
 (
-	account_id INTEGER,
-	full_name NVARCHAR(1000),
-	email_adress NVARCHAR(100),
-	phone_number NVARCHAR(100),
-	date_of_birth DATE,
-	gender NVARCHAR(100),
-	PRIMARY KEY (account_id)
+	AccountId INTEGER,
+	FullName NVARCHAR(1000),
+	EmailAddress NVARCHAR(100),
+	PhoneNumber VARCHAR(20),
+	DateOfBirth DATE,
+	Gender NVARCHAR(100),
+
+	PRIMARY KEY (AccountId)
 )
 
 CREATE TABLE PAYMENT_METHOD
 (
-	account_id INTEGER,
-	cardholder NVARCHAR(1000),
-	card_numver NVARCHAR(100),
-	expir_date NVARCHAR(10),
-	PRIMARY KEY (account_id)
+	AccountId INTEGER,
+	CardHolder NVARCHAR(1000),
+	CardNumber NVARCHAR(100),
+	ExpiryDate NVARCHAR(10),
+
+	PRIMARY KEY (AccountId)
 )
 
 CREATE TABLE BOOKING_ACCOUNT
 (
-	account_id INTEGER,
-	hotel_id INTEGER,
-	room_type INTEGER,
-	check_in DATE,
-	check_out DATE,
-	num_of_room INTEGER,
+	AccountId INTEGER,
+	HotelId INTEGER,
+	RoomTypeId INTEGER,
+	CheckIn DATE,
+	CheckOut DATE,
+	NumberOfRoom INTEGER,
 
-	PRIMARY KEY (hotel_id, room_type, check_in)
+	PRIMARY KEY (AccountId, HotelId, RoomTypeId, CheckIn)
 )
 
 
 CREATE TABLE CATEGORY
 (
-	category_name NVARCHAR(50),
-	sub_category NVARCHAR(50),
+	Name NVARCHAR(50),
+	SubCategory NVARCHAR(50),
 
-	PRIMARY KEY (category_name, sub_category)
+	PRIMARY KEY (Name, SubCategory)
 )
 
 CREATE TABLE REVIEW
 (
-	hotel_id INTEGER,
-	account_id INTEGER,
-	points FLOAT,
-	review NVARCHAR(1000),
+	AccountId INTEGER,
+	HotelId INTEGER,
+	Points FLOAT,
+	Content NVARCHAR(1000),
 
 	-- review chung cho khách sạn, có j sau này thêm thông tin phòng ở + các tiêu chí khác dô sau.
 	-- mỗi người đc 1 lần review, nếu review lại sẽ là chỉnh sửa. V cho nó dễ sài
 
-	PRIMARY KEY (hotel_id, account_id)
+	PRIMARY KEY (AccountId, HotelId)
+)
+
+CREATE TABLE REVIEW_IMAGE
+(
+	AccountId INTEGER,
+	HotelId INTEGER,
+	ImageId INT,
+	ImageLink VARCHAR(1000),
+
+
+	PRIMARY KEY (HotelId, AccountId, ImageId)
 )
 
 -------------------------------------------------
@@ -131,67 +144,79 @@ CREATE TABLE REVIEW
 ALTER TABLE ROOM_TYPE
 ADD
 	CONSTRAINT FK_ROOM_TYPE_HOTEL
-	FOREIGN KEY(hotel_id)
+	FOREIGN KEY(HotelId)
 	REFERENCES HOTEL
 
 ALTER TABLE ROOM_PICTURE
 ADD
 	CONSTRAINT FK_ROOM_PICTURE_ROOM_TYPE
-	FOREIGN KEY(hotel_id, room_type_id)
+	FOREIGN KEY(HotelId, RoomTypeId)
 	REFERENCES ROOM_TYPE
+
 --!Account
 ALTER TABLE PERSONAL_DETAILS
 ADD
 	CONSTRAINT FK_PERSONAL_DETAILS_ACCOUNT
-	FOREIGN KEY(account_id)
+	FOREIGN KEY(AccountId)
 	REFERENCES ACCOUNT
-
+	ON DELETE CASCADE
+	
 ALTER TABLE PAYMENT_METHOD
 ADD
 	CONSTRAINT FK_PAYMENT_METHOD_ACCOUNT
-	FOREIGN KEY(account_id)
+	FOREIGN KEY(AccountId)
 	REFERENCES ACCOUNT
-
-ALTER TABLE BOOKING_ACCOUNT
-ADD
-	CONSTRAINT FK_BOOKING_ACCOUNT_ROOM_TYPE
-	FOREIGN KEY(hotel_id, room_type)
-	REFERENCES ROOM_TYPE
 
 ALTER TABLE BOOKING_ACCOUNT
 ADD
 	CONSTRAINT FK_BOOKING_ACCOUNT_ACCOUNT
-	FOREIGN KEY(account_id)
+	FOREIGN KEY(AccountId)
 	REFERENCES ACCOUNT
+
+
+ALTER TABLE BOOKING_ACCOUNT
+ADD
+	CONSTRAINT FK_BOOKING_ACCOUNT_ROOM_TYPE
+	FOREIGN KEY(HotelId, RoomTypeId)
+	REFERENCES ROOM_TYPE
+
 
 --! đánh giá
 ALTER TABLE REVIEW
 ADD
 	CONSTRAINT FK_REVIEW_HOTEL
-	FOREIGN KEY(hotel_id)
+	FOREIGN KEY(HotelId)
 	REFERENCES HOTEL
+	ON DELETE CASCADE
 
 ALTER TABLE REVIEW
 ADD
 	CONSTRAINT FK_REVIEW_ACCOUNT
-	FOREIGN KEY(account_id)
+	FOREIGN KEY(AccountId)
 	REFERENCES ACCOUNT
+	ON DELETE CASCADE
 
+ALTER TABLE REVIEW_IMAGE
+ADD
+	CONSTRAINT FK_REVIEW_REVIEW_IMAGE
+	FOREIGN KEY(AccountId, HotelId)
+	REFERENCES REVIEW
+	ON DELETE CASCADE
 -------------------------------------------------
 
 --!add account
-INSERT INTO ACCOUNT VALUES (1, 'abc@gmail.com', 'abc123', 100);
-INSERT INTO ACCOUNT VALUES (2, 'bibizero@gmail.com', 'abc123', 45);
-INSERT INTO ACCOUNT VALUES (3, 'meme13@gmail.com', 'abc123', 20);
-INSERT INTO ACCOUNT VALUES (4, 'titanic9@gmail.com', 'abc123', 120);
-INSERT INTO ACCOUNT VALUES (5, 'gaianime@gmail.com', 'abc123', 60);
+--INSERT INTO ACCOUNT VALUES (1, 'abc@gmail.com', 'abc123', 100);
+--INSERT INTO ACCOUNT VALUES (2, 'bibizero@gmail.com', 'abc123', 45);
+--INSERT INTO ACCOUNT VALUES (3, 'meme13@gmail.com', 'abc123', 20);
+--INSERT INTO ACCOUNT VALUES (4, 'titanic9@gmail.com', 'abc123', 120);
+--INSERT INTO ACCOUNT VALUES (5, 'gaianime@gmail.com', 'abc123', 60);
 
---!add personal information
-INSERT INTO PERSONAL_DETAILS VALUES (1, N'Trần Văn A', 'abc@gmail.com', '0966188620', '1990-01-01', N'Nam');
-INSERT INTO PERSONAL_DETAILS VALUES (2, N'Nguyễn Văn B', 'bibizero@gmail.com', '0966188621', '1999-02-01', N'Nữ');
-INSERT INTO PERSONAL_DETAILS VALUES (3, N'Ngô Bá K', 'meme13@gmail.com', '0966188622', '1980-01-03', N'Nam');
-INSERT INTO PERSONAL_DETAILS VALUES (4, N'Lê C', 'titanic9@gmail.com', '0966188623', '1990-01-05', N'Nữ');
-INSERT INTO PERSONAL_DETAILS VALUES (5, N'Trần D', 'gaianime@gmail.com', '0966188624', '1990-01-06', N'Nam');
+----!add personal information
+--INSERT INTO PERSONAL_DETAILS VALUES (1, N'Trần Văn A', 'abc@gmail.com', '0966188620', '1990-01-01', N'Nam');
+--INSERT INTO PERSONAL_DETAILS VALUES (2, N'Nguyễn Văn B', 'bibizero@gmail.com', '0966188621', '1999-02-01', N'Nữ');
+--INSERT INTO PERSONAL_DETAILS VALUES (3, N'Ngô Bá K', 'meme13@gmail.com', '0966188622', '1980-01-03', N'Nam');
+--INSERT INTO PERSONAL_DETAILS VALUES (4, N'Lê C', 'titanic9@gmail.com', '0966188623', '1990-01-05', N'Nữ');
+--INSERT INTO PERSONAL_DETAILS VALUES (5, N'Trần D', 'gaianime@gmail.com', '0966188624', '1990-01-06', N'Nam');
 
 --!add hotel 
 INSERT INTO HOTEL VALUES(1, N'Triple E Hotel Metro Ben Thanh', N'Việt Nam', N'Hồ Chí Minh',
@@ -578,14 +603,6 @@ INSERT INTO ROOM_PICTURE VALUES(10,3,2,'https://atarceni.sirv.com/Shared/Sunrise
 INSERT INTO ROOM_PICTURE VALUES(10,3,3,'https://atarceni.sirv.com/Shared/Sunrise-Continent-from-rialloer/hotels/Crown%20Hotel%20Nha%20Trang/Ph%C3%B2ng%20Superior%20Gi%C6%B0%E1%BB%9Dng%20%C4%90%C3%B4i.2%20Gi%C6%B0%E1%BB%9Dng%20%C4%90%C6%A1n%20Nh%C3%ACn%20Ra%20Th%C3%A0nh%20Ph%E1%BB%913.jpg');
 
 
-
--- test review
-INSERT INTO REVIEW VALUES (1, 1, 5, N'Khách sạn đẹp đấy 1')
-INSERT INTO REVIEW VALUES (1, 2, 4, N'Khách sạn 2')
-INSERT INTO REVIEW VALUES (1, 3, 4, N'Khách sạn 2')
-
-INSERT INTO REVIEW VALUES (2, 1, 5, N'Khách sạn 3')
-INSERT INTO REVIEW VALUES (2, 2, 4, N'Khách sạn 2')
 
 
 
--- database/Procedure.sql
+++ database/Procedure.sql
@@ -1,118 +1,149 @@
-﻿USE Sunrise_continent_db
+﻿USE SunriseServer
 GO
 
-IF OBJECT_ID('USF_GetReviewNum') IS NOT NULL
-	DROP FUNCTION USF_GetReviewNum
-GO
-CREATE FUNCTION USF_GetReviewNum (@hotel_id INT)
+
+CREATE OR ALTER FUNCTION USF_GetReviewNum (@HotelId INT)
 RETURNS INT
 BEGIN
 	declare @Value INT
-	SET @Value = (SELECT COUNT(*) FROM REVIEW WHERE hotel_id = @hotel_id)
+	SET @Value = (SELECT COUNT(*) FROM REVIEW WHERE HotelId = @HotelId)
     RETURN @Value
 END
 GO
 
-IF OBJECT_ID('USF_GetAvgReview') IS NOT NULL
-	DROP FUNCTION USF_GetAvgReview
-GO
-CREATE FUNCTION USF_GetAvgReview (@hotel_id INT)
+
+CREATE OR ALTER FUNCTION USF_GetAvgReview (@HotelId INT)
 RETURNS FLOAT
 BEGIN
 	declare @Value FLOAT
-	SET @Value = (SELECT AVG(points) FROM REVIEW WHERE hotel_id = @hotel_id)
+	SET @Value = (SELECT AVG(Points) FROM REVIEW WHERE HotelId = @HotelId)
 	IF @Value IS NULL RETURN 0
     RETURN @Value
 END
 GO
 
-IF OBJECT_ID('USF_GetMinRoomPrice') IS NOT NULL
-	DROP FUNCTION USF_GetMinRoomPrice
-GO
-CREATE FUNCTION USF_GetMinRoomPrice (@hotel_id INT)
+
+CREATE OR ALTER FUNCTION USF_GetMinRoomPrice (@HotelId INT)
 RETURNS FLOAT
 BEGIN
 	declare @Value FLOAT
-	SET @Value = (SELECT MIN(price) FROM ROOM_TYPE WHERE hotel_id = @hotel_id)
+	SET @Value = (SELECT MIN(Price) FROM ROOM_TYPE WHERE HotelId = @HotelId)
 	IF @Value IS NULL RETURN 0
     RETURN @Value
 END
 GO
 
-IF OBJECT_ID('USP_GetAllAccommodations') IS NOT NULL
-	DROP PROC USP_GetAllAccommodations
-GO
-CREATE PROC USP_GetAllAccommodations 
+
+CREATE OR ALTER PROC USP_GetAllAccommodations 
 AS
-	SELECT hotel_id id, hotel_name as name, full_address as address, stars,
-			preview_img as preview,
-			dbo.USF_GetReviewNum(hotel_id) reviews, 
-			dbo.USF_GetAvgReview(hotel_id) points,
-			dbo.USF_GetMinRoomPrice(hotel_id) price
+	SELECT Id, Name, Address, Stars, Image,
+			dbo.USF_GetReviewNum(Id) Reviews, 
+			dbo.USF_GetAvgReview(Id) Points,
+			dbo.USF_GetMinRoomPrice(Id) Price
 	FROM HOTEL
 GO
 
-IF OBJECT_ID('USP_SearchByName') IS NOT NULL
-	DROP PROC USP_SearchByName
-GO
-CREATE PROC USP_SearchByName 
+
+CREATE OR ALTER PROC USP_SearchByName 
 	@search_query NVARCHAR(100)
 AS
-	IF NOT EXISTS (select * from HOTEL where hotel_description like (N'%' + @search_query + N'%'))
+	IF NOT EXISTS (select * from HOTEL where Description like (N'%' + @search_query + N'%'))
 	BEGIN
 		RETURN 0
 	END
 
-	SELECT hotel_id id, hotel_name as name, full_address as address, stars,
-			preview_img as preview,
-			dbo.USF_GetReviewNum(hotel_id) reviews, 
-			dbo.USF_GetAvgReview(hotel_id) points,
-			dbo.USF_GetMinRoomPrice(hotel_id) price
+	SELECT Id, Name, Address, Stars, Image,
+			dbo.USF_GetReviewNum(Id) Reviews, 
+			dbo.USF_GetAvgReview(Id) Points,
+			dbo.USF_GetMinRoomPrice(Id) Price
 	FROM HOTEL
-	WHERE hotel_description like (N'%' + @search_query + N'%')
+	WHERE Description like (N'%' + @search_query + N'%')
 
 	RETURN 1
 GO
 
-IF OBJECT_ID('USP_GetHotelRoomType') IS NOT NULL
-	DROP PROC USP_GetHotelRoomType
-GO
-CREATE PROC USP_GetHotelRoomType 
-	@hotel_id INT
+
+CREATE OR ALTER PROC USP_GetHotelRoomType 
+	@HotelId INT
 AS
-	IF NOT EXISTS (select * from HOTEL where hotel_id = @hotel_id)
+	IF NOT EXISTS (select * from HOTEL where Id = @HotelId)
 	BEGIN
 		RETURN 0
 	END
 
 	select * from ROOM_TYPE rt
-	where rt.hotel_id = @hotel_id
+	where rt.HotelId = @HotelId
 
 	RETURN 1
 GO
 
-IF OBJECT_ID('USP_GetHotelRoomPicture') IS NOT NULL
-	DROP PROC USP_GetHotelRoomPicture
-GO
-CREATE PROC USP_GetHotelRoomPicture 
-	@hotel_id INT,
-	@room_id INT
+CREATE OR ALTER PROC USP_GetHotelRoomPicture 
+	@HotelId INT,
+	@RoomId INT
 AS
-	IF NOT EXISTS (select * from HOTEL where hotel_id = @hotel_id)
+	IF NOT EXISTS (select * from HOTEL where Id = @HotelId)
 	BEGIN
 		RETURN 0
 	END
 
 	select * from ROOM_PICTURE rp
-	where rp.hotel_id = @hotel_id and rp.room_type_id = @room_id
+	where rp.HotelId = @HotelId and rp.RoomTypeId = @RoomId
 
 	RETURN 1
 GO
 
---IF OBJECT_ID('USF_GetReviewNumAndPoints') IS NOT NULL
---	DROP FUNCTION USP_GetReviewNumAndPoints
---GO
---CREATE FUNCTION USP_GetReviewNumAndPoints ()
+CREATE OR ALTER PROC USP_GetAllHotel
+AS
+	SELECT * FROM HOTEL
+GO
+
+
+CREATE OR ALTER PROC USP_GetHotelById
+	@Id INTEGER
+AS
+	SELECT * FROM HOTEL WHERE Id = @Id
+GO
+
+
+CREATE OR ALTER PROC USP_AddAccount
+	@Id INTEGER,
+	@MemberPoints INTEGER,
+	@Username VARCHAR(50),
+	@PasswordHash VARBINARY(100),
+	@PasswordSalt VARBINARY(100),
+	@RefreshToken VARCHAR(100),
+	@TokenCreated DATETIME,
+	@TokenExpires DATETIME
+AS
+	IF EXISTS (SELECT * FROM ACCOUNT WHERE Id=@Id)
+	BEGIN
+		PRINT N'Id tồn tại'
+		RETURN 0
+	END
+
+	INSERT INTO ACCOUNT VALUES (@Id, @MemberPoints, @Username, @PasswordHash, @PasswordSalt, @RefreshToken, @TokenCreated, @TokenExpires)
+
+	RETURN 1
+GO
+
+CREATE OR ALTER PROC USP_DeleteAccount
+	@Id INTEGER
+AS
+	BEGIN TRY
+		
+
+		DELETE FROM ACCOUNT WHERE Id = @Id
+		RETURN 0
+	END TRY
+
+	begin catch
+		print N'Account deletion error'
+		return 1
+	end catch
+GO
+
+
+--CREATE OR ALTER FUNCTION USP_GetReviewNumAndPoints ()
 --RETURNS @ReviewAndPoints TABLE (hotel_id INT)
 --BEGIN
 --	return 
@@ -123,12 +154,3 @@ GO
 declare @res INT
 --exec @res = USP_SearchByName @search_query = N'hotel'
 --print @res
-
-
---exec @res = USP_GetHotelRoomType @hotel_id = 2
---print @res
-
-exec @res = USP_GetHotelRoomPicture @hotel_id = 2, @room_id = 1
-print @res
-
---print dbo.USF_GetReviewNum(2)

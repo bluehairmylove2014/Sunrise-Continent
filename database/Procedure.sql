2216bb0b8760b3418e7d505b1875a4bea2dbad13
cuong457 <pnccuong20@clc.fitus.edu.vn>
2023-07-07 17:02:05 +0700

    Update: add Create, Update, Delete procedure for ACCOUNT table

--- database/Procedure.sql
+++ database/Procedure.sql
@@ -105,9 +105,42 @@ AS
 GO
 
 
+--drop function dbo.USF_GetNextAccountId
+
+CREATE OR ALTER PROC USP_GetNextAccountId(
+	@tablename SYSNAME,
+	@columnname SYSNAME
+)
+AS
+	DECLARE @IntVariable INT = 0;  
+	DECLARE @SQLString NVARCHAR(MAX);  
+	DECLARE @ParmDefinition NVARCHAR(500);
+  
+	SET @SQLString = 
+		N'with cte as (select ' + @columnname + ' id, lead(' + @columnname + ') over (order by ' + @columnname + ') nextid from ' + @tablename + ')
+		select @gapstartOUT = MIN(id) from cte
+		where id < nextid - 1';
+	SET @ParmDefinition = N'@gapstartOUT INTEGER OUTPUT';  
+  
+	EXECUTE sp_executesql @SQLString, @ParmDefinition, @gapstartOUT = @IntVariable OUTPUT;  
+
+	IF (@IntVariable IS NULL)
+	BEGIN
+		DECLARE @SQL NVARCHAR(MAX);
+		SET @SQL = N'select @IdOUT = count(' + @columnname + ') from ' + @tablename + '';
+		SET @ParmDefinition = N'@IdOUT INTEGER OUTPUT';
+		EXEC sp_executesql @SQL, @ParmDefinition, @IdOUT = @IntVariable OUTPUT;
+
+		RETURN @IntVariable;
+	END
+
+	RETURN @IntVariable + 1; 
+GO
+
+
 CREATE OR ALTER PROC USP_AddAccount
-	@Id INTEGER,
-	@MemberPoints INTEGER,
+	--@Id INTEGER,
+	--@MemberPoint INTEGER,
 	@Username VARCHAR(50),
 	@PasswordHash VARBINARY(100),
 	@PasswordSalt VARBINARY(100),
@@ -115,17 +148,106 @@ CREATE OR ALTER PROC USP_AddAccount
 	@TokenCreated DATETIME,
 	@TokenExpires DATETIME
 AS
-	IF EXISTS (SELECT * FROM ACCOUNT WHERE Id=@Id)
-	BEGIN
-		PRINT N'Id tồn tại'
+	BEGIN TRY
+		DECLARE @Id INT
+		EXEC @Id = USP_GetNextAccountId 'ACCOUNT', 'Id'
+
+		INSERT INTO ACCOUNT VALUES (@Id, 0, @Username, @PasswordHash, @PasswordSalt, @RefreshToken, @TokenCreated, @TokenExpires)
 		RETURN 0
-	END
+	END TRY
 
-	INSERT INTO ACCOUNT VALUES (@Id, @MemberPoints, @Username, @PasswordHash, @PasswordSalt, @RefreshToken, @TokenCreated, @TokenExpires)
-
-	RETURN 1
+	BEGIN CATCH
+		PRINT N'Account insertion error'
+		RETURN 1
+	END CATCH
 GO
 
+--declare @Test1 VARBINARY(100)
+--SET @Test1 = CONVERT(VARBINARY(100), '0x9473FBCCBC01AF', 1)
+--exec dbo.USP_AddAccount
+--	@Username = 'hahaha31247', 
+--	@PasswordHash = @Test1, 
+--	@PasswordSalt = @Test1, 
+--	@RefreshToken = 'VCL', 
+--	@TokenCreated = '07-21-2023', 
+--	@TokenExpires = '07-21-2026';
+--GO
+
+--Select * from ACCOUNT
+
+--delete from ACCOUNT where Username like 'hahaha31247'
+--delete from ACCOUNT where Id in (3, 5)
+
+--DECLARE @IntVariable INT = -10;
+--DECLARE @SQL NVARCHAR(MAX)
+--DECLARE @ParmDefinition NVARCHAR(500);
+--SET @SQL = N'select @IdOUT = count(Id) from ACCOUNT';
+--SET @ParmDefinition = N'@IdOUT INTEGER OUTPUT';
+--EXEC sp_executesql @SQL, @ParmDefinition, @IdOUT = @IntVariable OUTPUT;
+--IF (@IntVariable IS NULL)
+--	PRINT N'Vờ lờ'
+--ELSE
+--	PRINT @IntVariable
+
+--DECLARE @res INT
+--exec @res = USP_GetNextAccountId 'ACCOUNT', 'Id'
+--print @res
+
+--select count (Id) from ACCOUNT
+
+--GO
+
+CREATE OR ALTER PROC USP_UpdateAccount
+	@Id INTEGER,
+	@MemberPoint INTEGER,
+	@Username VARCHAR(50),
+	@PasswordHash VARBINARY(100),
+	@PasswordSalt VARBINARY(100),
+	@RefreshToken VARCHAR(100),
+	@TokenCreated DATETIME,
+	@TokenExpires DATETIME
+AS
+	BEGIN TRY
+		IF NOT EXISTS (SELECT * FROM ACCOUNT WHERE Id = @Id)
+		BEGIN
+			PRINT N'Account Id existed'
+			RETURN 1
+		END
+
+		UPDATE ACCOUNT 
+		SET
+			MemberPoint = @MemberPoint,
+			Username = @Username,
+			PasswordHash = @PasswordHash,
+			PasswordSalt = @PasswordSalt,
+			RefreshToken = @RefreshToken,
+			TokenCreated = @TokenCreated,
+			TokenExpires = @TokenExpires
+		WHERE Id = @Id
+		RETURN 0
+	END TRY
+
+	BEGIN CATCH
+		PRINT N'Account update error'
+		RETURN 1
+	END CATCH
+GO
+
+--declare @Test1 VARBINARY(100)
+--SET @Test1 = CONVERT(VARBINARY(100), '0x9473FBCCBC01AF', 1)
+--exec dbo.USP_UpdateAccount 
+--	@Id = 3,
+--	@MemberPoint = 10,
+--	@Username = 'xinchao', 
+--	@PasswordHash = @Test1, 
+--	@PasswordSalt = @Test1, 
+--	@RefreshToken = 'VCL', 
+--	@TokenCreated = '07-21-2023', 
+--	@TokenExpires = '07-21-2026';
+--GO
+
+
+GO
 CREATE OR ALTER PROC USP_DeleteAccount
 	@Id INTEGER
 AS
@@ -136,21 +258,38 @@ AS
 		RETURN 0
 	END TRY
 
-	begin catch
-		print N'Account deletion error'
-		return 1
-	end catch
+	BEGIN CATCH
+		PRINT N'Account deletion error'
+		RETURN 1
+	END CATCH
 GO
 
 
---CREATE OR ALTER FUNCTION USP_GetReviewNumAndPoints ()
---RETURNS @ReviewAndPoints TABLE (hotel_id INT)
---BEGIN
---	return 
---END
---GO
+--exec dbo.USP_DeleteAccount @Id = 5
+--select * from ACCOUNT
 
---exec USP_GetAllAccommodations
-declare @res INT
---exec @res = USP_SearchByName @search_query = N'hotel'
---print @res
+
+----CREATE OR ALTER FUNCTION USP_GetReviewNumAndPoints ()
+----RETURNS @ReviewAndPoints TABLE (hotel_id INT)
+----BEGIN
+----	return 
+----END
+----GO
+
+
+
+----exec USP_GetAllAccommodations
+----exec @res = USP_SearchByName @search_query = N'hotel'
+----print @res
+
+
+--select * from ACCOUNT
+
+--declare @res INT
+--set @res = (select MAX(Id) from ACCOUNT)
+
+----if exists (select @res = MAX(Id) from ACCOUNT)
+--Go
+
+--print dbo.USF_GetNextAccountId()
+

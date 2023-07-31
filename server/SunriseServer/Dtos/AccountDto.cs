ac6f07740aeff28bdbb724aadba5fb55f6c3bf0f
Phuc Dat <bluehairmylove1014@gmail.com>
2023-07-31 15:42:38 +0700

    update yml 7

--- server/SunriseServer/Dtos/AccountDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SunriseServer.Dtos
-{
-    public class AccountDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
---
+++ src/components/common/WistList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../styles/scss/_common_component.scss";
+
+const WistList = () => {
+  return <sidebar className="common-component__wishlist"></sidebar>;
+};
+
+export default WistList;
